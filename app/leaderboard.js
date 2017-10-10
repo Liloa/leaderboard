b627402af99663fd3482cdd901c54fe14b7830be
Liloa <dalocalman@gmail.com>
2017-10-10 11:24:13 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,32 @@
-console.log("Hello world");
+if(Meteor.isClient){
+  console.log("Hello client");
+
+
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'otherHelperFunction': function(){
+      return "Some other function";
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+
+}
+if(Meteor.isServer){
+  console.log("Hello server");
+}
+PlayersList = new Mongo.Collection('players');
